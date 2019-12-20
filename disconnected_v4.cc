2f21a4504f9937530300e5af7e48c953764524c1
Rui Zhang <zhangrui9308@gmail.com>
2019-12-20 17:31:34 -0500

    reduce output in log

--- disconnected_v4.cc
+++ disconnected_v4.cc
@@ -1255,7 +1255,7 @@ int main(int argc, char **argv) {
 			
 			for (int d=0; d<num_disp; ++d){
 				int disp=link_patterns[d];
-				if(Layout::primaryNode()) std::cout << "calculating link "<< d << " in direction "<< disp <<std::endl;
+				//if(Layout::primaryNode()) std::cout << "calculating link "<< d << " in direction "<< disp <<std::endl;
 				
 				shift_link(d,chi,psi,shift_psi,num_disp_mom,num_disp,disp,U);
 				
@@ -1416,7 +1416,7 @@ int main(int argc, char **argv) {
 				
 				for (int d=0; d<num_disp; ++d){
 					int disp=link_patterns[d];
-					if(Layout::primaryNode()) std::cout << "calculating link "<< d << " in direction "<< disp <<std::endl;
+					//if(Layout::primaryNode()) std::cout << "calculating link "<< d << " in direction "<< disp <<std::endl;
 					shift_link(d,chi,psi,shift_psi,num_disp_mom,num_disp,disp,U);
 					
 //					if(d<num_disp_mom){
@@ -1498,7 +1498,7 @@ int main(int argc, char **argv) {
 				for (int d=0; d<num_disp; ++d){
 					int disp=link_patterns[d];
 					
-					if(Layout::primaryNode()) std::cout << "calculating link "<< d << " in direction "<< link_patterns[d] <<std::endl;
+					//if(Layout::primaryNode()) std::cout << "calculating link "<< d << " in direction "<< link_patterns[d] <<std::endl;
 					
 					shift_link(d,chi,psi,shift_psi,num_disp_mom,num_disp,disp,U);
 					
