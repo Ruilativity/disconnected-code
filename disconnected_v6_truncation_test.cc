5ef40ad9a89a52bbcb86bd83e94705000c04b4f6
Rui Zhang <zhangrui9308@gmail.com>
2021-05-02 23:36:30 -0400

    print all LP results to one file

--- disconnected_v6_truncation_test.cc
+++ disconnected_v6_truncation_test.cc
@@ -576,7 +576,7 @@ int main(int argc, char **argv) {
 			}
 		}
 		else{
-			for(int t_shift=0;t_shift<=Layout::lattSize()[Nd-1]-t_src;t_shift++){
+			for(int t_shift=0;t_shift<=Layout::lattSize(:QDP)[Nd-1]-t_src;t_shift++){
 				for(int mu=0;mu<Nd-1;mu++){
 				U[mu]=shift(U_tmp[mu], FORWARD, Nd-1);// shift the whole lattice BWD so that the source is located at t=0.
 				U_tmp[mu]=U[mu];
@@ -997,8 +997,8 @@ int main(int argc, char **argv) {
 	<< std::endl;
 	
 	QDPIO::cout << "DISCO: ran successfully" << std::endl;
-	QDPIO::cerr << "Successfully Run " << std::endl;
-	QDP_abort(1);
+	QDPIO::cout << "Successfully Run " << std::endl;
+	//QDP_abort(1);
 	END_CODE();
 	
 	Chroma::finalize();
